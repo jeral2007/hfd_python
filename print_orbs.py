b53a55aabab4451a9912832af72a41dc696af36a
jeral2007 <jeral2007@gmail.com>
2016-04-18 18:54:15 +0300

    electron_integrals tested

--- print_orbs.py
+++ print_orbs.py
@@ -3,8 +3,9 @@
 import sys
 import hfd_in_out
 
+
 def data_shell(orbs):
-    return [orb for orb in orbs if orb['kp'] <0.5 #unfrozen shells only
+    return [orb for orb in orbs if orb['kp'] < 0.5]  # unfrozen shells only
 
 hfdres, shells = sys.argv[1], sys.argv[2:]
 orbs, en = hfd_in_out.orbitals(open(hfdres, 'r'))
@@ -12,7 +13,7 @@ orbs, en = hfd_in_out.orbitals(open(hfdres, 'r'))
 unfrozen = data_shell(orbs)
 print len(unfrozen)
 
-for data in data_shell(orbs, shells):
+for data in data_shell(orbs):
     print("{}{}\t{}".format(data['nl'], data['j'],
                             str(data['en']).replace('.', ',')))
 print(en)
--- test_electron_integrals.py
+++ test_electron_integrals.py
@@ -4,6 +4,7 @@ import sys
 from source_hfd_python.hfd_dat import Hfd
 from scipy import trapz, exp, where
 
+
 def str2nlj(s):
     n = int(s[0])
     l = 'spdfgh'.index(s[1])
@@ -12,7 +13,8 @@ def str2nlj(s):
 
 hfd = Hfd()
 grid = (hfd.grid, hfd.weights, hfd.h)
-nc = where(hfd.grid>0.5)[0][0]
+nc = where(hfd.grid > 0.5)[0][0]
+print(nc)
 testgrid = exp(-hfd.grid)
 res = trapz(testgrid[:nc]*hfd.weights[:nc])*hfd.h
 print("test grid")
@@ -20,16 +22,49 @@ print("{} - {} = {}\n".format(res, (1-exp(-0.5)), res-1.0+exp(-0.5)))
 rc = 5e-1
 csn = sys.argv[1]  # core shell
 vsns = sys.argv[2:]  # valent shells
-nc, lc, jc = str2nlj(csn)
+ncc, lc, jc = str2nlj(csn)
 p, q, en = hfd[csn]
-
+print("norm function")
 core_orb = (p, q, lc, jc)
+norm2rc = trapz((p[:nc]**2+q[:nc]**2)*hfd.weights[:nc])*hfd.h
+norm2 = trapz((p**2+q**2)*hfd.weights[:hfd.imax])*hfd.h
+print("||{}||^2  = {}\t{}".format(csn, norm2rc, norm2))
+v_orbs = []
+for v in vsns:
+    nv, lv, jv = str2nlj(v)
+    pv, qv, env = hfd[v]
+    v_orb = (pv, qv, lv, jv)
+    v_orbs += [v_orb]
+    coul = el.coulumb(core_orb, v_orb, grid, rc)
+    exc = -el.exchange(core_orb, v_orb, grid, rc)
+    out = "{} {}\t{:.6f}\t{:.6f}".format(csn, v, coul, exc)
+    print out.replace('.', ',')
+# grid info
+with open('grid.txt', 'w') as outp:
+    for i, r in enumerate(hfd.grid):
+        outp.write("{} {}\n".format(i, r))
+# orb info
+with open('orbs.txt', 'w') as outp:
+    r = grid[0]
+    for i in xrange(hfd.imax):
+        res = "\t".join(str(v_orb[0][i]) for v_orb in v_orbs)
+        outp.write("{}\t{}\n".format(r[i], res))
+
+print("electron_melem_test")
+
+
+def test_with_orb(corb, vorb, v, grid, rc):
+
+    res = "{}\t{}\t{}\t{}\t{}".format(v, el.coulumb(core_orb, v_orb, grid, rc),
+                                      el.coulumb_melem(core_orb, v_orb, v_orb,
+                                                       grid, rc),
+                                      el.exchange(core_orb, v_orb, grid, rc),
+                                      el.exchange_melem(core_orb, v_orb, v_orb,
+                                                        grid, rc))
+    print(res.replace('.', ','))
 
 for v in vsns:
     nv, lv, jv = str2nlj(v)
     pv, qv, env = hfd[v]
     v_orb = (pv, qv, lv, jv)
-    coul = el.coulumb(core_orb, v_orb, grid, rc)
-    exc = el.exchange(core_orb, v_orb, grid, rc)
-    out = "{} {}\t{:.6f}\t{:.6f}".format(csn, v, coul, exc)
-    print out.replace('.',',')
+    test_with_orb(core_orb, v_orb, v, grid, rc)
