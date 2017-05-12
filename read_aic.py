2d8e47c605540d8d3a4a975d0b7f43b9778b1f09
jeral2007 <jeral2007@gmail.com>
2017-05-12 20:34:46 +0300

    read_aic.py is for reading oneProp huge output and pickling it to the one file

---
+++ read_aic.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+import glob
+import sys
+import cPickle
+
+def parse_prop_file(pfile):
+    for line in pfile:
+        if 'value Re' not in line or any(sym*2 in line for sym in 'SPDFGH'):
+            continue
+        aux = line.split('=')
+        val = aux[1]
+        pname = aux[0].split('(')[1][:-1]
+        return pname, val
+
+
+try:
+    prop_path, dump_file = sys.argv[1]
+except Exception:
+    sys.stderr.write("""\
+Usage:
+    {} folder dump
+folder is  folder of OneProp output (log/JReduced0.55 for example)\n
+dump is dump file with props""".format(sys.argv[0]))
+
+    sys.exit(1)
+
+prop_prefix = "JRed_prop_ljm"
+props = []
+for prop_fname in glob.glob(prop_path + '/' + prop_prefix + '*'):
+    with open(prop_fname, 'r') as pfile:
+        props += [parse_prop_file(pfile)]
+
+dumpf = open(dump_file, 'w')
+cPickle.dump(props, dumpf)
+
+print props
