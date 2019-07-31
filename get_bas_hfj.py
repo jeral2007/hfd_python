1f6f545859ef3de8cd2455963ee68f1257b069be
jeral <jeral@xeon2.(none)>
2019-07-31 06:23:38 +0400

    sdsdsd

---
+++ get_bas_hfj.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python2.7
+#coding:utf8
+
+from __future__ import print_function
+from source_hfd_python.hfd_dat import Hfj
+from source_hfd_python.makebas import Basis
+import sys
+import os.path
+
+basfile, orbs = sys.argv[1], sys.argv[2:]
+hfd = Hfj('HFJ.DAT')
+if not os.path.isfile(basfile):
+    bas = Basis(hfd)
+else:
+    bas = Basis.load_bas(basfile)
+
+for orb in orbs:
+    bas.add_orbital(hfd, orb)
+
+print(str(bas))
+bas.save_bas(basfile)
